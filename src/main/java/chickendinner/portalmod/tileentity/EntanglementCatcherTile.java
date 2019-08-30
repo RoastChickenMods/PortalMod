976f77378b8ae56cb2a6a180cfec57bc29b09486
Luke Gilfoyle <boo122333@gmail.com>
2019-08-30 16:24:25 +0100

    Add inventory to entanglement catcher tile.
    The entanglement catcher can only store entangled pairs.
    
    Signed-off-by: Luke Gilfoyle <boo122333@gmail.com>

--- src/main/java/chickendinner/portalmod/tileentity/EntanglementCatcherTile.java
+++ src/main/java/chickendinner/portalmod/tileentity/EntanglementCatcherTile.java
@@ -2,20 +2,38 @@ package chickendinner.portalmod.tileentity;
 
 import chickendinner.portalmod.PortalMod;
 import chickendinner.portalmod.tileentity.base.MachineTile;
-import chickendinner.portalmod.tileentity.module.EnergyModule;
 import chickendinner.portalmod.tileentity.module.IModule;
+import chickendinner.portalmod.tileentity.module.ItemModule;
 import com.google.common.collect.ImmutableSet;
-import net.minecraft.tileentity.TileEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.items.ItemStackHandler;
 
+import javax.annotation.Nonnull;
 import java.util.Set;
 
 public class EntanglementCatcherTile extends MachineTile {
+    private final ItemStackHandler itemStorage;
+
     public EntanglementCatcherTile() {
         super(PortalMod.Tiles.ENTANGLEMENT_CATCHER);
+        itemStorage = new ItemStackHandler(1) {
+            @Override
+            public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+                if (slot == 0) {
+                    return stack.getItem() == PortalMod.Items.ENTANGLED_PAIR; // TODO: use a tag
+                }
+                return super.isItemValid(slot, stack);
+            }
+
+            @Override
+            protected void onContentsChanged(int slot) {
+                EntanglementCatcherTile.this.markDirty();
+            }
+        };
     }
 
     @Override
     protected void addModules(Set<IModule> modules) {
-
+        modules.add(new ItemModule(ImmutableSet.of(getFacing()), itemStorage));
     }
 }
